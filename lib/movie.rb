ff55a6e5dbe30e2de3e914c59616772975d2f9aa
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-07-27 18:46:47 -0500

    Created framwork for desired output using case statement

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,6 +1,18 @@
 class MovieData
+    attr_accessor :title, :year
+
     def initialize(title, year = nil)
         @title = title
         @year = year if year
+    end
+
+
+    case input
+    when "genre" then display_genre
+    when "rating" then display_rating
+    when "plot" then display_plot
+    when "full title" then display_full_title
+    when "year" then display_year
+    when "runtime" then display_runtime
     end
 end
