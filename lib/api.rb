e01ff58b73b335ad404f1b69adda8429b4324998
jrdriscoll17 <jrdriscoll17@gmail.com>
2020-07-29 14:47:41 -0500

    Added error handling for invalid name and/or year to API class

--- lib/api.rb
+++ lib/api.rb
@@ -7,13 +7,19 @@ class API
         omdb_url = "http://www.omdbapi.com/?apikey=1f082bdb&t=#{title}&#{formatted_year}"
         response = HTTParty.get(omdb_url)
 
-        attributes = {
-            :title => response["Title"],
-            :year => response["Year"],
-            :genre => response["Genre"],
-            :rating => response["Ratings"][0]["Value"],
-            :plot => response["Plot"],
-            :runtime => response["Runtime"]
-        }
+        if response["Error"]
+            response["Error"]
+        else
+            attributes = {
+                :title => response["Title"],
+                :year => response["Year"],
+                :genre => response["Genre"],
+                :rating => response["Ratings"][0]["Value"],
+                :plot => response["Plot"],
+                :runtime => response["Runtime"]
+            }
+        end
+
+
     end
 end
