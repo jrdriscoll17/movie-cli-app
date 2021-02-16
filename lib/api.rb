ba45219737804ea1c3f38f917df579cf52f1fba2
Jake <jrdriscoll17@gmail.com>
2021-02-16 08:27:45 -0600

    Cleaned up API class

--- lib/api.rb
+++ lib/api.rb
@@ -5,22 +5,13 @@ class API
         omdb_url = "http://www.omdbapi.com/?apikey=1f082bdb&t=#{title}&#{formatted_year}"
         response = HTTParty.get(omdb_url)
 
-        if response["Error"]
-            response
-        else
-            attributes = {
-                :title => response["Title"],
-                :year => response["Year"],
-                :genre => response["Genre"],
-                :plot => response["Plot"],
-                :runtime => response["Runtime"]
-            }
-            if response["Ratings"] == []
-                attributes[:rating] = "N/A"
-            else
-                attributes[:rating] = response["Ratings"][0]["Value"]
-            end
-        end
-        attributes
+        response["Error"] ? response : attributes = {
+            :title => response["Title"],
+            :year => response["Year"],
+            :genre => response["Genre"],
+            :plot => response["Plot"],
+            :runtime => response["Runtime"],
+            :rating => response["imdbRating"]
+        }
     end
 end
